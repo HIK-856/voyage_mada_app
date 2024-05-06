28abfce209cf844498e83188b1f41f778a75d2ba
ihantsa <ihantsa01@gmail.com>
2024-05-06 22:38:27 +0300

    geolocalisation

--- lib/Controller/Geolocalisation.dart
+++ lib/Controller/Geolocalisation.dart
@@ -10,9 +10,7 @@ class Geolocalisation  {
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
+      
       return Future.error('Location services are disabled.');
     }
 
@@ -20,11 +18,7 @@ class Geolocalisation  {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
+        
         return Future.error('Location permissions are denied');
       }
     }
